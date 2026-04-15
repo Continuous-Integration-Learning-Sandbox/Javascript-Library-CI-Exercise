5f232fdc5d806f7386e0a400039ec8211dc06294
Armand-Veress <armand.veress@student.unitbv.ro>
2026-04-15 15:02:51 +0300

    feat: add toKebabCase and fix license field in package.json

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const toSnakeCase = (str) => {
         .join('_');
 };
 
-module.exports = { toSnakeCase };
+const toKebabCase = (str) => {
+    return str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.toLowerCase())
+        .join('-');
+};
+
+module.exports = { toSnakeCase, toKebabCase };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const { toSnakeCase } = require('./index');
+const { toSnakeCase, toKebabCase } = require('./index');
 
 test('should convert string to snake_case', () => {
     expect(toSnakeCase('Hello World')).toBe('hello_world');
+});
+
+test('should convert string to kebab-case', () => {
+    expect(toKebabCase('Hello World')).toBe('hello-world');
 });
